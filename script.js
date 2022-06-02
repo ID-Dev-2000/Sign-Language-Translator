73b040473b724b5a2a1a4646e660380b0147e181
ID <izaakduncan@outlook.com>
2022-06-01 19:59:38 -0700

    space functionality added

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ userInputButton.addEventListener("click", arrayToImage)
 userResetButton.addEventListener("click", clearAll)
 
 // On-enterpress Event Listener
-receivedUserText.addEventListener("keydown", function(event){
-    if (event.key === "Enter"){
+receivedUserText.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
         arrayToImage()
     }
 })
@@ -24,19 +24,28 @@ function clearAll() {
 // Use regex to generate clean string, then split string to array
 function stringToArray() {
     let userTextValue = receivedUserText.value
-    let regex = /[^a-zA-Z]/g
-    userTextValue = userTextValue.replace(regex,'')
-    return userTextValue.split("")
+    let cleanString = /[^a-zA-Z ]/g
+    userTextValue = userTextValue.replace(cleanString,'') // removes all non alphabetic characters
+    return userTextValue.split('')
 }
 
 // Takes array conversion, assigns each letter to image, displays image in DOM
 function arrayToImage() {
     let arrayOfUserData = stringToArray()
     aSLImageDiv.innerHTML = ''
-    for (let [key, value] of Object.entries(arrayOfUserData)){
-        let stringArrayData = `IMAGES/ASL-${value.toUpperCase()}.PNG`
-        let imageFromArrayData = document.createElement('img')
-        imageFromArrayData.src = stringArrayData
-        aSLImageDiv.appendChild(imageFromArrayData)
+
+    // Converts array to string, regex to handle space, set back to array
+    // Handles spaces in output sentence
+    for (i=0; i<arrayOfUserData.length; i++) {
+            stringOfUserData = arrayOfUserData.toString()
+            cleanStringOfUserDataWithCommas = stringOfUserData.replace(/[\s]/g, '1')
+            cleanStringOfUserDataFinal = cleanStringOfUserDataWithCommas.replace(/,/g, '')
+            cleanArrayOfUserData = cleanStringOfUserDataFinal.split('')
+    }
+        for (let [key, value] of Object.entries(cleanArrayOfUserData)) {
+            let stringArrayData = `IMAGES/ASL-${value.toUpperCase()}.PNG`
+            let imageFromArrayData = document.createElement('img')
+            imageFromArrayData.src = stringArrayData
+            aSLImageDiv.appendChild(imageFromArrayData)
     }
 }
